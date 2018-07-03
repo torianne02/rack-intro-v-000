49a7cbae82aa1db1dc2dcfcade824839ba4e5edc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-03 20:00:03 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is Victoria"
+    resp.write "Hello, my name is Victoria."
     resp.finish
   end
 
